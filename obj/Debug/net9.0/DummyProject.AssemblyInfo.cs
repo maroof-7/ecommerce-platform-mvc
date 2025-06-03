53111ff07eb68b45ebdcc09fa4501e869f00dfc2
Your Name <you@example.com>
2025-06-03 11:33:32 +0530

    first commit

--- obj/Debug/net9.0/DummyProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/DummyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DummyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0595ddccdd71fd7a0d68a8b955425be6046473e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DummyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DummyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
